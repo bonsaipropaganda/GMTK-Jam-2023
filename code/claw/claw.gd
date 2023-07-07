26ae2ea62fbcb8848601f0f27e55028e56bd8940
Khusheete <Siandfrance@gmail.com>
2023-07-07 23:47:34 +0200

    Fixed animation bug (had to not sync arm AnimatableBody to physics) & Updated grab_distance

--- code/claw/claw.gd
+++ code/claw/claw.gd
@@ -12,7 +12,7 @@ extends AnimatableBody2D
 ## The speed at wich the claw goes back up
 @export var ascending_speed: float = 200.0
 ## The horizontal distance at wich the claw tries to grab
-@export var grab_distance: float = 35.0
+@export var grab_distance: float = 45.0
 
 
 ## If the player is inside this area, close the hand
