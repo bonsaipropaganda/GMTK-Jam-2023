a27c3dd34400db14472105c84de6b2db6fe5c590
brandloose <133024462+brandloose@users.noreply.github.com>
2023-07-07 22:23:02 -0400

    Made the claw drift stop

--- code/claw/claw.gd
+++ code/claw/claw.gd
@@ -1,7 +1,7 @@
 class_name Claw
 extends AnimatableBody2D
 
-const SLOW_DOWN_SPEED = 1
+const SLOW_DOWN_SPEED = 5
 
 @export var target: Node2D # TOOD: change to player type
 
@@ -15,7 +15,6 @@ const SLOW_DOWN_SPEED = 1
 ## The horizontal distance at which the claw tries to grab
 @export var grab_distance: float = 20
 
-
 ## If the player is inside this area, close the hand
 @onready var hand_area: Area2D = $HandArea
 
@@ -27,12 +26,18 @@ var set_ascending_speed: float= ascending_speed
 
 func _physics_process(delta):
 	if $"../Player".burrowed:
-		if (homing_speed != 0):
+		if (homing_speed > 0):
 			homing_speed -= SLOW_DOWN_SPEED
-		if (descending_speed != 0):
+		else:
+			homing_speed = 0;
+		if (descending_speed > 0):
 			descending_speed -= SLOW_DOWN_SPEED
-		if (ascending_speed != 0):
+		else:
+			descending_speed = 0;
+		if (ascending_speed > 0):
 			ascending_speed -= SLOW_DOWN_SPEED
+		else:
+			ascending_speed = 0;
 	else:
 		homing_speed = set_homing_speed
 		descending_speed = set_descending_speed
