f999c6589285015c6df476774e713a71d9ca19d6
Khusheete <Siandfrance@gmail.com>
2023-07-08 17:53:49 +0200

    Added settings menu (for audio) and placeholder music to test it

--- code/GUI/main_menu.gd
+++ code/GUI/main_menu.gd
@@ -1,5 +1,8 @@
 extends Control
 
+
+var SettingsMenu: PackedScene = preload("res://scenes/GUI/settings.tscn")
+
 @export var main_game_scene: PackedScene
 
 
@@ -9,3 +12,8 @@ func _on_start_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+func _on_settings_buttion_pressed() -> void:
+	var settings: Node = SettingsMenu.instantiate()
+	add_child(settings)
