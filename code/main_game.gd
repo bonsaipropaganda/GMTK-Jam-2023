2fd01abfdd32b6acba05c37d3a0074456fadd39d
bonsaipropaganda <sethvihanlouis@gmail.com>
2023-07-08 11:23:20 -0500

    added quit button and main scene. added quit feature to main game. Press esc to quit

--- code/main_game.gd
+++ code/main_game.gd
@@ -1,6 +1,5 @@
-extends Control
+extends Node2D
 
-@export var main_game_scene: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,10 +10,6 @@ func _ready():
 func _process(delta):
 	pass
 
-
-func _on_start_button_pressed():
-	get_tree().change_scene_to_packed(main_game_scene)
-
-
-func _on_quit_button_pressed():
-	get_tree().quit()
+func _input(event):
+	if event.is_action_pressed("ui_end"):
+		get_tree().quit()
