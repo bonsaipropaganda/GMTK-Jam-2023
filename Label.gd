453b37346da7433f5e4cb45ca2c4268a007520bd
bonsaipropaganda <sethvihanlouis@gmail.com>
2023-07-09 16:34:39 -0500

    my janky room system

--- Label.gd
+++ Label.gd
@@ -1,6 +1,6 @@
 extends Label
 
-@export var time_left:= 60
+@export var time_left:= 30
 signal level_passed
 
 # Called when the node enters the scene tree for the first time.
@@ -10,13 +10,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass
+
+
+func _on_level_timer_timeout():
 	self.text = "Time left: " + str(time_left)
+	time_left -= 1
+	$LevelTimer.start()
 	
 	if time_left <= 0:
 		$LevelTimer.stop()
 		level_passed.emit()
-
-
-func _on_level_timer_timeout():
-	time_left -= 1
-	$LevelTimer.start()
+		time_left = 30
+		$LevelTimer.start()
