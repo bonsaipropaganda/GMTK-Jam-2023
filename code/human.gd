56c2ed2fef7eff9c1796d4cef659384bbcbf20ba
Khusheete <Siandfrance@gmail.com>
2023-07-09 10:17:37 +0200

    Fixed passing shadow collision when coming from the right

--- code/human.gd
+++ code/human.gd
@@ -4,17 +4,13 @@ var walk_direction: String = "right"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	#Set the animation according to the direction
-	if(walk_direction == "right"):
+	# Set the animation according to the direction
+	if walk_direction == "right":
 		$AnimationPlayer.current_animation = "Pass_LeftToRight"
 	else:
 		$AnimationPlayer.current_animation = "Pass_RightToLeft"
 		$Sprite2D.set_flip_h(true)
+		$CollisionPolygon2D.scale.x = -1.0
 	
 	$AnimationPlayer.play()
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
 	
